0b7bbe32eb02d1bb3a319810f2dbe6db268dfb7f
bluematter <michael@motionbox.io>
2022-02-04 08:05:12 -0800

    fetch prisma data

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import babel from "rollup-plugin-babel";
 import ts from "rollup-plugin-typescript2";
 import replace from "@rollup/plugin-replace";
 import pkg from "./package.json";
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const PLUGINS = [
   ts({
@@ -11,6 +13,8 @@ const PLUGINS = [
     extensions: [".ts", ".js", ".tsx", ".jsx"],
     presets: ['@babel/env']
   }),
+  nodeResolve({browser: true}),
+  commonjs(),
   replace({
     _VERSION: JSON.stringify(pkg.version),
   }),
@@ -21,7 +25,7 @@ export default [
     input: "src/index.ts",
     output: [
       { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
+      { file: pkg.module, format: "es" }
     ],
     plugins: PLUGINS,
   }
---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+describe("Motionbox integration", () => {
+  test("Motionbox integration code is working", () => {
+    expect(1).toEqual(1);
+  });
+});
+
+export {};
